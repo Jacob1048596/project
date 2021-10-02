14cce1ec3eb981472be621f7654ce2bf3bd95819
Jacob1048596 <qinsheng2580@foxmail.com>
2021-10-02 22:22:31 +0800

    10.2

---
+++ IM/im.cpp
@@ -0,0 +1,20 @@
+#include "im.hpp"
+void sql_test()
+{
+	// im::TableUser user;
+	//user.Insert("zhangsa","123456");
+	//user.UpdatePasswd("zhangsan","111111");
+	//cout<<"1"<<endl;
+	//cout<<user.exists("lis")<<endl;
+	// Json::Value User;
+	// user.SelectAll(&User);
+	// Json::StyledWriter wri;
+	// cout<<wri.write(User)<<endl;
+	
+	
+}
+int main()
+{
+	sql_test();
+	return 0;
+}
